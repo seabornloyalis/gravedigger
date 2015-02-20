94be0c78ce6d2ed5d5b65ee932a4becfbd78f0b4
Perditian <david.taus@tufts.edu>
2015-02-19 23:59:42 -0500

    Attempt to add movement failed

--- Gravedigger/Assets/Scripts/GameManager.cs
+++ Gravedigger/Assets/Scripts/GameManager.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	public float turnDelay = .1f;
+	public static GameManager instance = null;
 	private BoardManager boardScript;
+	//public int playerHealth = 10;
+	//stored values from the player between levels go here
+	[HideInInspector] public bool playersTurn = true;
+
 	private int level = 1;
 
 	void Awake() {
---
+++ Gravedigger/Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject 
+{
+
+	public int digPower = 1;
+	public int attack = 1;
+
+	public int health = 10; // will be changed to private after tweaked
+	
+	// Use this for initialization
+	protected override void Start () 
+	{
+		base.Start ();
+	}
+
+	private void OnDisable()
+	{
+		//will be used to save data between levels
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!GameManager.instance.playersTurn) {
+			return;
+		}
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)Input.GetAxisRaw ("Horizontal");
+		vertical = (int)Input.GetAxisRaw ("Vertical");
+
+		if (horizontal != 0) {
+			vertical = 0;
+		}
+		//May need to rework this to determine what objects would be collided with
+		// either obstacle or enemy
+		if (horizontal != 0 || vertical != 0) {
+			AttemptMove<Enemy>(horizontal, vertical);
+		}
+	}
+
+	protected override void AttemptMove<T> (int xDir, int zDir)
+	{
+		base.AttemptMove<T> (xDir, zDir);
+
+		RaycastHit hit;
+
+		if (Move (xDir, zDir, out hit)) {
+		
+		}
+
+		GameManager.instance.playersTurn = false;
+	}
+
+	private void OnTrigerEnter(Collider other)
+	{
+		//CODE HERE FOR INTERACTING WITH OBJECTS
+	}
+
+	protected override void OnCantMove<T> (T component)
+	{
+		Enemy hitEnemy = component as Enemy;
+		health -= hitEnemy.damage;
+		CheckIfGameOver ();
+		hitEnemy.attacked(attack);
+		//MAYBE CAN USE THE "as" OPERATOR TO HANDLE MULTIPLE CASES
+	}
+
+	private void Restart()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	private void CheckIfGameOver()
+	{
+		if (health <= 0) {
+			//GameManager.instance.GameOver ();
+		}
+	}
+}
